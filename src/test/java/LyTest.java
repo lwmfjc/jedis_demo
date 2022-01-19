e03ae3f574f95f965ac86a82338804eac2df9b6c
lwmfjc <lwmfjc@gmail.com>
2022-01-19 17:30:50 +0800

    getcode

--- src/test/java/LyTest.java
+++ src/test/java/LyTest.java
@@ -2,6 +2,8 @@ import org.junit.Test;
 import redis.clients.jedis.DefaultJedisClientConfig;
 import redis.clients.jedis.Jedis;
 
+import java.util.Random;
+
 public class LyTest {
     private Jedis getRedis() {
         //设置密码
@@ -14,7 +16,15 @@ public class LyTest {
     }
 
     @Test
-    private void getCode() {
-
+    public String getCode() {
+        int i = new Random().nextInt(10000);
+        StringBuilder builder=new StringBuilder();
+        if(i<10){
+            return "000"+i;
+        }else if(i<100){
+            return "00"+i;
+        }
+        System.out.println(i);
+        return builder.toString();
     }
 }
